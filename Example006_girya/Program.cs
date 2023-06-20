66d3b60fa53cc089a32886c20d5af374afda37e6
Danila_Zh <denis_petrovich_02@bk.ru>
2023-06-21 01:18:54 +0500

    Girya_ves

---
+++ Example006_girya/Program.cs
@@ -0,0 +1,16 @@
+﻿int a =1;
+int d =9;
+int s =6;
+int b =8;
+int e =2;
+
+int max = a;
+
+if (a> max) max = a;
+if (d> max) max = d;
+if (s> max) max = s;
+if (b> max) max = b;
+if (e> max) max = e;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
