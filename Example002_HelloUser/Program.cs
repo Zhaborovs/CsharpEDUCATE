a11253bc9f2daab680f3972648ad6d0a543db4fc
Danila_Zh <denis_petrovich_02@bk.ru>
2023-06-21 00:21:56 +0500

    Create Eximple002_user

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine(); //строка переменноая имя = ввод с консоли
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
